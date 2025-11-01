30b49b879ab8456bc41fb4d7cf552740d0dded65
alshansky-g <rgcs.rgcs@gmail.com>
2025-11-01 19:08:45 +0300

    add product model

--- goods/admin.py
+++ goods/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from goods.models import Category
+from goods.models import Category, Product
 
 admin.site.register(Category)
+admin.site.register(Product)
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 
 
@@ -9,3 +11,25 @@ class Category(models.Model):
         db_table = 'categories'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(
+        upload_to='goods_images', blank=True, null=True, verbose_name='Изображение'
+    )
+    price = models.DecimalField(
+        default=Decimal(0.00), max_digits=7, decimal_places=2, verbose_name='Цена'
+    )
+    discount = models.DecimalField(
+        default=Decimal(0.00), max_digits=7, decimal_places=2, verbose_name='Скидка в %'
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
