bd6cac0e9478fe01fb1f295757a4bfaf9bf30ac9
alshansky-g <rgcs.rgcs@gmail.com>
2025-11-15 21:50:21 +0300

    refactor login form, improve form errors handling

--- users/forms.py
+++ users/forms.py
@@ -5,27 +5,8 @@ from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label='Имя пользователя',
-        widget=forms.TextInput(
-            attrs={
-                'autofocus': True,
-                'class': 'form-control',
-                'placeholder': 'Введите ваше имя пользователя',
-            }
-        ),
-    )
-    password = forms.CharField(
-        label='Пароль',
-        widget=forms.PasswordInput(
-            attrs={
-                'autocomplete': 'current-password',
-                'class': 'form-control',
-                'placeholder': 'Введите ваш пароль',
-            }
-        ),
-    )
-
     class Meta:
         model = User
         fields = ('username', 'password')
+
+    password = forms.CharField(label='Пароль')
