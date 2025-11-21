b8872fa1ff39c36df8c85d91f95ac66dfc64014e
alshansky-g <rgcs.rgcs@gmail.com>
2025-11-21 22:05:17 +0300

    delete order button with empty cart

--- carts/urls.py
+++ carts/urls.py
@@ -6,6 +6,6 @@ app_name = 'cart'
 
 urlpatterns = [
     path('cart-add/<slug:product_slug>/', cart_add, name='cart_add'),
-    path('cart-change/<slug:product_slug>/', cart_change, name='cart_change'),
-    path('cart-remove/<slug:product_slug>/', cart_remove, name='cart_remove'),
+    path('cart-change/<int:cart_id>/', cart_change, name='cart_change'),
+    path('cart-remove/<int:cart_id>/', cart_remove, name='cart_remove'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -19,7 +19,12 @@ def cart_add(request, product_slug):
     return redirect(request.META['HTTP_REFERER'])
 
 
-def cart_change(request, product_slug): ...
+def cart_change(request, cart_id):
+    ...
+    # cart = Cart.objects.get(id=cart_id)
+    # return redirect(request.META['HTTP_REFERER'])
 
 
-def cart_remove(request, product_slug): ...
+def cart_remove(request, cart_id):
+    Cart.objects.get(id=cart_id).delete()
+    return redirect(request.META['HTTP_REFERER'])
