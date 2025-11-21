c3a9e82d1a0a0dcd16b697a076e8fd84fc8dafc7
alshansky-g <rgcs.rgcs@gmail.com>
2025-11-21 23:33:09 +0300

    add cart dynamic update

--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -1,12 +1,10 @@
 from django import template
 
-from carts.models import Cart
+from carts.utils import get_user_carts
 
 register = template.Library()
 
 
 @register.simple_tag()
 def user_carts(request):
-    if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user).select_related('product')
-    return None  # TODO: для неавторизованного пользователя
+    return get_user_carts(request)
--- carts/urls.py
+++ carts/urls.py
@@ -5,7 +5,7 @@ from carts.views import cart_add, cart_change, cart_remove
 app_name = 'cart'
 
 urlpatterns = [
-    path('cart-add/<slug:product_slug>/', cart_add, name='cart_add'),
-    path('cart-change/<int:cart_id>/', cart_change, name='cart_change'),
-    path('cart-remove/<int:cart_id>/', cart_remove, name='cart_remove'),
+    path('cart-add/', cart_add, name='cart_add'),
+    path('cart-change/', cart_change, name='cart_change'),
+    path('cart-remove/', cart_remove, name='cart_remove'),
 ]
