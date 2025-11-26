4aaec7170cba52f8b0f7e610d1956d2de117047a
alshansky-g <rgcs.rgcs@gmail.com>
2025-11-26 18:35:47 +0300

    refactor: goods app now using CBV

--- goods/views.py
+++ goods/views.py
@@ -1,8 +1,44 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
+from django.views.generic import DetailView, ListView
 
 from goods.models import Product
 from goods.utils import q_search
+
+
+class CatalogView(ListView):
+    model = Product
+    template_name = 'goods/catalog.html'
+    context_object_name = 'products'
+    paginate_by = 3
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug')
+        on_sale = self.request.GET.get('on_sale', None)
+        order_by = self.request.GET.get('order_by', None)
+        query = self.request.GET.get('q', None)
+
+        if category_slug == 'all':
+            products = super().get_queryset()
+        elif query:
+            products = q_search(query)
+        else:
+            products = super().get_queryset().filter(category__slug=category_slug)
+
+        if on_sale:
+            products = products.filter(discount__gt=0)
+        if order_by and order_by != 'default':
+            products = products.order_by(order_by)
+
+        return products
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': 'Home - Каталог',
+            'slug_url': self.kwargs.get('category_slug'),
+        })
+        return context
 
 
 def catalog(request, category_slug: str | None = None):
@@ -35,7 +71,16 @@ def catalog(request, category_slug: str | None = None):
     return render(request, 'goods/catalog.html', context=context)
 
 
-def product(request, product_slug: str):
-    product = Product.objects.get(slug=product_slug)
-    context = {'product': product}
-    return render(request, 'goods/product.html', context=context)
+class ProductView(DetailView):
+    template_name = 'goods/product.html'
+    slug_url_kwarg = 'product_slug'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'title': self.object.name})
+        return context
+
+    def get_object(self) -> Product:
+        product = Product.objects.get(slug=self.kwargs.get(self.slug_url_kwarg))
+        return product
