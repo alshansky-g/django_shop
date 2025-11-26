4c2e6e5b552115a563159762b53f4136596a99c0
alshansky-g <rgcs.rgcs@gmail.com>
2025-11-26 16:24:41 +0300

    refactor main app using CBV

--- main/views.py
+++ main/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def index(request):
-    context = {
-        'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME',
-    }
-    return render(request, 'main/index.html', context=context)
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': 'Home - Главная',
+            'content': 'Магазин мебели HOME',
+        })
+        return context
 
 
-def about(request):
-    context = {
-        'title': 'Home - О нас',
-        'content': 'О нас',
-        'text_on_page': 'Тут информация о нас, наших ценностях и миссии.',
-    }
-    return render(request, 'main/about.html', context=context)
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': 'Home - О нас',
+            'content': 'О нас',
+            'text_on_page': 'Тут информация о нас, наших ценностях и миссии.',
+        })
+        return context
