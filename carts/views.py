0a4ab99712aa417d8e3148cc156f6403ed84be20
alshansky-g <rgcs.rgcs@gmail.com>
2025-11-26 15:55:51 +0300

    fix order button appearing when changing product quantity

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.urls import reverse
 
 from carts.models import Cart
 from carts.utils import get_user_carts
@@ -48,8 +49,12 @@ def cart_change(request):
     cart.save()
 
     user_cart = get_user_carts(request)
+    context = {'carts': user_cart}
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context['order'] = True
     cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': user_cart}, request=request
+        'carts/includes/included_cart.html', context, request=request
     )
     response_data = {
         'message': 'Количество изменено',
@@ -66,8 +71,12 @@ def cart_remove(request):
     cart.delete()
 
     user_cart = get_user_carts(request)
+    context = {'carts': user_cart}
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context['order'] = True
     cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': user_cart}, request=request
+        'carts/includes/included_cart.html', context, request=request
     )
     response_data = {
         'message': 'Товар удалён из корзины',
